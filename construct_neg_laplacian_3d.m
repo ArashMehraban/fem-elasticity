93149d60735d4a2e2efa5965b396d4488efb1cf2
Arash Mehraban <arashm81@gmail.com>
2016-06-01 21:43:34 -0600

    construct the negative of Laplacian of u for 3D

--- construct_neg_laplacian_3d.m
+++ construct_neg_laplacian_3d.m
@@ -1,7 +1,7 @@
 function neg_laplacian_3d = construct_neg_laplacian_3d()
 %CONSTRUCT_NEG_LAPLACIAN_3D returns the negative Laplacian of u to be used
 %in get_userf function
-% input: none (inside this function)
+% input: none (inside this function below)
 % output: the negative laplacian of u below 
     u=@(x,y,z)tanh(x).*exp(y)+sin(y)+cos(z);
     
@@ -11,5 +11,4 @@ function neg_laplacian_3d = construct_neg_laplacian_3d()
     d2z = diff(u(x,y,z),z,2);
     poss3D = simplify(-(d2x+d2y+d2z));
     neg_laplacian_3d = matlabFunction(poss3D);
-
 end
