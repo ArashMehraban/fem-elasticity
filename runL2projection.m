8e11d60e2f9b9b5601afcff3352ac264182c39d5
Arash Mehraban <arashm81@gmail.com>
2016-06-02 18:38:33 -0600

    run to solve L2 Proj in 2D and 3D with Quad4, Quad9, HEX8 and HE27

--- runL2projection.m
+++ runL2projection.m
@@ -1,106 +1,112 @@
-%+++++++++
-% Old L2 projection. Must be updated with new code!!
-%++++++++
-
-%Solving the L2 projection problem using FEM
+%Solving L2 Projection problem using FEM with unstructured mesh produced by Cubit
+% version 12.1
 clear
 clc
 format short
 
-%mesh refinement
-%vector of errors
-j=1;
-err=zeros(1,12);
-h = zeros(1,12);
-for i=6:5:11 %6:5:66  %66
-    % User given parameters to generate mesh with quadliteral elements
-    nx=i;
-    ny=i;
-    nz=i+2; 
-    x0 = -pi/2;
-    y0 = -pi/2;
-    z0 = -pi/2;
-    x1 = pi/2;
-    y1 = pi/2;
-    z1 = pi/2;
-    elm_type = 'Q1';
-    
-  
-    % generate 2D mesh based on element type
-    if (strcmp(elm_type,'Q1') || strcmp(elm_type,'Q2'))
-        [conn,vtx_coords,~,~] = create_mesh(elm_type, nx,x0,x1,ny, y0,y1);
-    end
-    % generate 3D mesh based on element type
-    if (strcmp(elm_type,'Q1H') || strcmp(elm_type,'Q2H'))
-        [conn,vtx_coords,~,~] = create_mesh(elm_type, nx,x0,x1,ny, y0,y1,nz,z0,z1);
-    end
-    
-    [conn,vtx_coords] = applybd2mesh(conn,vtx_coords,-1,-1);
-    
-    % number of unknowns/nodes on the mesh
-    u_sz = size(vtx_coords,1);
-    % solution vector
-    u = zeros(1,u_sz)';
-     
-    norms = zeros(1,12);
-    
-    global_res_norm=1;
-   
-    iter=1;
-    norm_iter=1;
-    while((global_res_norm > 1.0e-9 || fdglobal_res_norm > 1.0e-9) && iter <3 )
+file_name_list{1} = {'disk4_114e_us', 'disk4_169e_us', 'disk4_274e_us', 'disk4_641e_us','disk4_2598e_us' };
+file_name_list{2} = {'disk9_114e_us', 'disk9_169e_us', 'disk9_274e_us', 'disk9_641e_us','disk9_2598e_us' };
+file_name_list{3} = {'cylinder8_110e_us', 'cylinder8_368e_us', 'cylinder8_1176e_us', ...
+                     'cylinder8_1635e_us','cylinder8_4180e_us' };
+file_name_list{4} = {'cylinder27_64e_us', 'cylinder27_110e_us', 'cylinder27_368e_us', ...
+                     'cylinder27_630e_us','cylinder27_1428e_us' };
+
+folderName = 'mesh_no_ns_ss';
+
+addpath(fullfile(pwd,folderName));
+
+files = cell(file_name_list);
+for i=1:size(file_name_list,2)
+    files{i} = get_files(folderName, file_name_list{i});
+end
+
+
+for i=1:size(files,2)
+    error = zeros(1,size(files{i},2));
+    h = zeros(1,size(files{i},2)); 
+    for j=1:size(files{i},2)        
+        filenames = files{i}{j};
+        ext = 'exo';
+        msh = get_mesh(filenames,ext,'lex');
+        dims = msh.num_dims;
+        elem_type = msh.num_nodes_per_elem;
+        h(j) = get_hsz(msh);
+         
+        sz_u_field = 1;
         
-        [global_res, jac] = eval_res(u, conn,vtx_coords, elm_type,1,1);
-                
-        global_res_norm = norm(global_res);
-        if(global_res_norm < 1.0e-9)
-            break;
+        %get all Dirichlet boundary node sets
+        dir_bndry_nodes = get_all_dir_ns(msh);
+
+        %NOTE: modify userf function according to given_u
+        if(dims==2)
+            given_u{1}=@(x,y)tanh(x).*exp(y)+sin(y);
+        elseif(dims==3)
+            given_u{1}=@(x,y,z)tanh(x).*exp(y)+sin(y)+cos(z);
         end
 
-        norms(norm_iter)= global_res_norm;         
-        
-        fun = @(u)eval_res(u, conn, vtx_coords, elm_type,1,1);
-               
-        options = optimoptions(@fsolve,'Algorithm','trust-region-reflective','Jacobian','on');
-        %options = optimset('Jacobian','on');
-        sol = fsolve(fun, global_res, options);        
-        u=sol;
-        
-        norm_iter=norm_iter+1;
-        iter=iter+1;
-    end  
-      
-    fem_sol = sol;
-    
-    exactSol = exactf(vtx_coords);
-        
-    % element size (hsz)
-    if (strcmp(elm_type,'Q1') || strcmp(elm_type,'Q2') )                         
-        hsz = sqrt( ((x1-x0)/nx)^2 + ((y1-y0)/ny)^2);
-    end
-    if (strcmp(elm_type,'Q1H') || strcmp(elm_type,'Q2H') )                         
-        hsz = sqrt(((x1-x0)/nx)^2 + ((y1-y0)/ny)^2 +((z1-z0)/nz)^2);
-    end
+        %Construct manufactured solution:
+        %========================================================================================%
+        %get vertex coordinates from mesh                                     
+        vtx_coords = msh.vtx_coords;                                          
+        %get constructed dir_bndry_vals and exac Solutions on remaining nodes 
+        [dir_bndry_val, exactSol] = get_exact_sol(vtx_coords,dir_bndry_nodes, given_u);
+        %========================================================================================%
 
-    %calculate the error norm
-    error = norm(exactSol - fem_sol)/norm(fem_sol);
-    err(j) =error;
-    h(j) = hsz;
-    j=j+1;
-    
+        fem_sol =  get_sol(msh, sz_u_field, dir_bndry_nodes, dir_bndry_val);
+        error(j) = norm(exactSol - fem_sol)/norm(fem_sol);
+    end
+    if(dims ==2)
+        prb_title = 'L2 projection: 2D';
+        if(elem_type == 4)
+            leg_enry_1 = 'FEM-QUAD4';
+            lglg_factor_1 = 0.001;
+            lglg_pwr_1 = h;
+            lglg_factor_2 = 0.1;
+            lglg_pwr_2 = h.^2;
+            leg_enry_2 = 'O(h)';
+            leg_enry_3 = 'O(h^2)';
+        elseif(elem_type == 9)
+            leg_enry_1 = 'FEM-QUAD9';
+            lglg_factor_1 = 0.00001;
+            lglg_pwr_1 = h.^2;
+            lglg_factor_2 = 0.001;
+            lglg_pwr_2 = h.^3;
+            leg_enry_2 = 'O(h^2)';
+            leg_enry_3 = 'O(h^3)';
+        end
+    elseif(dims ==3)
+        prb_title = 'L2 projection: 3D';
+        if(elem_type == 8)
+            leg_enry_1 = 'FEM-HEX8';
+            lglg_factor_1 = 0.001;
+            lglg_pwr_1 = h;
+            lglg_factor_2 = 0.1;
+            lglg_pwr_2 = h.^2;
+            leg_enry_2 = 'O(h)';
+            leg_enry_3 = 'O(h^2)';
+        elseif(elem_type == 27)
+            leg_enry_1 = 'FEM-HEX27';
+            lglg_factor_1 = 0.0001;
+            lglg_pwr_1 = h.^2;
+            lglg_factor_2 = 0.001;
+            lglg_pwr_2 = h.^3;        
+            leg_enry_2 = 'O(h^2)';
+            leg_enry_3 = 'O(h^3)';
+        end
+    end
+    subplot(2,2,i);
+    loglog(h,error,'r-o', h,lglg_factor_1*(lglg_pwr_1),'r:',h, lglg_factor_2*(lglg_pwr_2),'b--');
+    legend(leg_enry_1,leg_enry_2,leg_enry_3,'Location','northwest')
+    title(prb_title)
+    xlabel('h')
+    ylabel('error')    
 end
-figure
-% Q1
-loglog(h,err,'r-o', h,0.001*h,'r:',h, 0.01*(h.^2),'b--');
-legend('FEM-Q1','O(h)','O(h^2)','Location','northwest')
-title('L2-proj: 25-4225 Q1 (2D) elements')
 
-% % Q2
-% loglog(h,err,'r-o', h,0.001*(h.^3),'r:',h, 0.1*(h.^2),'b--');
-% legend('FEM-Q2','O(h^3)','O(h^2)','Location','northwest')
-% title('L2-proj: 25-4225 Q2 (2D) elements')
 
-xlabel('h = sqrt(sum(rectangular element sides squared))')
-ylabel('error')
+rmpath(fullfile(pwd,folderName));
+    
+
+
+
 
 
