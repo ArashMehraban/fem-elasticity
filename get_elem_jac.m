3c92a4b077660cd9ba1be54657d3789f88425cc2
Arash Mehraban <arashm81@gmail.com>
2016-06-09 19:58:53 -0600

    returns element Jacobian

--- get_elem_jac.m
+++ get_elem_jac.m
@@ -19,11 +19,9 @@ function  [detsJe, invJe] = get_elem_jac(elem_vtx_coords, Ds)
   end
 
   JD0 = D0*elem_vtx_coords;
-  JD0=JD0(:);
-      
+        
   JD1 = D1*elem_vtx_coords;
-  JD1=JD1(:);
-
+  
   num_gs_pts = size(D0,1);
 
   invJe = zeros(D_sz*num_gs_pts,D_sz); 
@@ -32,13 +30,13 @@ function  [detsJe, invJe] = get_elem_jac(elem_vtx_coords, Ds)
   %2D
    
   % structure of Je per quadrature point:
-  % Je = [partial_x/partial_xi , partial_x/partial_eta]
-  %      [partial_y/partial_xi , partial_y/partial_eta]
+  % Je = [partial_x/partial_xi  , partial_y/partial_xi]
+  %      [partial_x/partial_eta , partial_y/partial_eta]
     
    if(D_sz == 2 )        
       idx = 2*(1:num_gs_pts)-1;
       for i=1:num_gs_pts
-          tmpJe = [JD0(i:num_gs_pts:end),JD1(i:num_gs_pts:end)];
+          tmpJe = [JD0(i,:); JD1(i,:)];
           invJe(idx(i):idx(i)+D_sz-1,:)=inv(tmpJe);
           detsJe(i)=det(tmpJe);
           if(detsJe(i) < 0)
@@ -46,28 +44,35 @@ function  [detsJe, invJe] = get_elem_jac(elem_vtx_coords, Ds)
           end
       end       
    end
+  % structure of inVJe per quadrature point:
+  % invJe = [partial_x/partial_xi  ,partial_x/partial_eta]
+  %         [partial_y/partial_xi , partial_y/partial_eta]
    
+   
+  
   %3D
   
   % structure of Je per quadrature point:
-  % Je = [partial_x/partial_xi , partial_x/partial_eta, partial_x/partial_zeta]
-  %      [partial_y/partial_xi , partial_y/partial_eta, partial_y/partial_zeta]
-  %      [partial_z/partial_xi , partial_z/partial_eta, partial_z/partial_zeta]
+  % Je = [partial_x/partial_xi ,  partial_y/partial_xi,   partial_z/partial_xi  ]
+  %      [partial_x/partial_eta , partial_y/partial_eta,  partial_z/partial_eta ]
+  %      [partial_x/partial_zeta, partial_y/partial_zeta, partial_z/partial_zeta]
   if(D_sz == 3)            
       JD2 = D2*elem_vtx_coords;
-      JD2=JD2(:);
-      
+            
       idx = 3*(1:num_gs_pts)-2;
       for i=1:num_gs_pts
-          tmpJe = [JD0(i:num_gs_pts:end),JD1(i:num_gs_pts:end),JD2(i:num_gs_pts:end)];
+          tmpJe = [JD0(i,:); JD1(i,:); JD2(i,:)];
           invJe(idx(i):idx(i)+D_sz-1,:)=inv(tmpJe);
           detsJe(i)=det(tmpJe);
           if(detsJe(i) < 0)
               error('Defective element! Negative determinant in element Jacobian');
           end
       end
-
-  end      
+  end    
+   % structure of invJe per quadrature point
+    % invJe = [partial_xi/partial_x , partial_eta/partial_x, partial_zeta/partial_x]
+    %         [partial_xi/partial_y , partial_eta/partial_y, partial_zeta/partial_y]
+    %         [partial_xi/partial_z , partial_eta/partial_z, partial_zeta/partial_z]
 end  
 
 
