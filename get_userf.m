c199d2cd511a5881080f71de0899276731dfcba1
Arash Mehraban <arashm81@gmail.com>
2016-06-09 20:02:16 -0600

    provides weak form of the problem to solve

--- get_userf.m
+++ get_userf.m
@@ -25,76 +25,186 @@ function [f0, f1,f00, f01, f10, f11] = get_userf(ue, grad_ue, xe)
 
 %==== L2 Starts==================================%    
      
-   % For L2-Projection problem:
-   % Weak Form: (v: test/weight function)
-   % integral(v*f0(ue,grad_ue) + grad_v : f1(ue,grad_ue)) = 0   
-      
-      
-    %user defined rhs
-    %2D
-    if(sz_dim == 2)   
-       g = tanh(x).*exp(y)+sin(y);
-       f0{1} = ue - g; 
-      
-       f1=cell(1,size(grad_ue,2));
-       for  j=1:size(grad_ue,2)
-           f1{j} = 0*grad_ue{j};
-       end
-       
-       
-       %f_0,i = partial(f_i)/partial(ue)
-       f00{1} = ones(size(f0{1},1),1);
-       f01{1} = zeros(size(f1{1},1),1); 
-       f01{2} = zeros(size(f1{1},1),1);
-       
-       
-       %f_1,i = partial(f_i)/partial(gradue)
-       f10{1} = zeros(size(f0{1},1),1);
-       f10{2} = zeros(size(f0{1},1),1);
-       f11{1} = zeros(size(f1{1},1),1);
-       f11{2} = zeros(size(f1{1},1),1);  
-    end
-    
-    %3D
-    if(sz_dim == 3)
-       g = tanh(x).*exp(y)+sin(y)+cos(z);
-       f0{1} = ue - g; 
-      
-       f1=cell(1,size(grad_ue,2));
-       for  j=1:size(grad_ue,2)
-           f1{j} = 0*grad_ue{j};
-       end
-              
-       %f_0,i = partial(f_i)/partial(ue)
-       f00{1} = ones(size(f0{1},1),1);
-       f01{1} = zeros(size(f1{1},1),1); 
-       f01{2} = zeros(size(f1{1},1),1);
-       f01{3} = zeros(size(f1{1},1),1);
-       
-       
-       %f_1,i = partial(f_i)/partial(gradue)
-       f10{1} = zeros(size(f0{1},1),1);
-       f10{2} = zeros(size(f0{1},1),1);
-       f10{3} = zeros(size(f0{1},1),1);
-       f11{1} = zeros(size(f1{1},1),1);
-       f11{2} = zeros(size(f1{2},1),1); 
-       f11{3} = zeros(size(f1{3},1),1);
-       
-    end
+% %    % For L2-Projection problem:
+% %    % Weak Form: (v: test/weight function)
+% %    % integral(v*f0(ue,grad_ue) + grad_v : f1(ue,grad_ue)) = 0   
+% %       
+% %       
+% %     %user defined rhs
+% %     %2D
+% %     if(sz_dim == 2)   
+% %        g = tanh(x).*exp(y)+sin(y);
+% %        f0{1} = ue - g; 
+% %       
+% %        f1=cell(1,size(grad_ue,2));
+% %        for  j=1:size(grad_ue,2)
+% %            f1{j} = 0*grad_ue{j};
+% %        end
+% %        
+% %        
+% %        %f_0,i = partial(f_i)/partial(ue)
+% %        f00{1} = ones(size(f0{1},1),1);
+% %        f01{1} = zeros(size(f1{1},1),1); 
+% %        f01{2} = zeros(size(f1{1},1),1);
+% %        
+% %        
+% %        %f_1,i = partial(f_i)/partial(gradue)
+% %        f10{1} = zeros(size(f0{1},1),1);
+% %        f10{2} = zeros(size(f0{1},1),1);
+% %        f11{1} = zeros(size(f1{1},1),1);
+% %        f11{2} = zeros(size(f1{1},1),1);  
+% %     end
+% %     
+% %     %3D
+% %     if(sz_dim == 3)
+% %        g = tanh(x).*exp(y)+sin(y)+cos(z);
+% %        f0{1} = ue - g; 
+% %       
+% %        f1=cell(1,size(grad_ue,2));
+% %        for  j=1:size(grad_ue,2)
+% %            f1{j} = 0*grad_ue{j};
+% %        end
+% %               
+% %        %f_0,i = partial(f_i)/partial(ue)
+% %        f00{1} = ones(size(f0{1},1),1);
+% %        f01{1} = zeros(size(f1{1},1),1); 
+% %        f01{2} = zeros(size(f1{1},1),1);
+% %        f01{3} = zeros(size(f1{1},1),1);
+% %        
+% %        
+% %        %f_1,i = partial(f_i)/partial(gradue)
+% %        f10{1} = zeros(size(f0{1},1),1);
+% %        f10{2} = zeros(size(f0{1},1),1);
+% %        f10{3} = zeros(size(f0{1},1),1);
+% %        f11{1} = zeros(size(f1{1},1),1);
+% %        f11{2} = zeros(size(f1{2},1),1); 
+% %        f11{3} = zeros(size(f1{3},1),1);
+% %        
+% %     end
 
 %==== L2 ends==================================%
 
 
 %==== Poisson Starts==================================%
-% % %      
-% % %    if(sz_dim == 2)
+      
+%    if(sz_dim == 2)
+%      %2D  
+%      %Poisson eqn:
+%      g=sin(y)+exp(y).*tanh(x)-exp(y).*tanh(x).^3.*2.0;
+%      %==== -\nabla^2 (u) = g  with 0 Dirichlet B.C. ====%
+%      f0{1} =  0*ue -g; 
+%      f1{1} = grad_ue{1};
+%      f1{2} = grad_ue{2};
+%      %=====================================================%
+%        
+%      %f0 = @(ue, grad_ue) ue-g;
+%      %f1 = @(ue, grad_ue) grad_ue;
+% 
+%      %f_i,0 = partial(f_i)/partial(ue)
+%      f00{1} = zeros(size(f0{1},1),1);
+%      f10{1} = zeros(size(f1{1},1),1); 
+%      f10{2} = zeros(size(f1{1},1),1); 
+%      
+%      %f_i,1 = partial(f_i)/partial(gradue)
+%      f01{1} = zeros(size(f0{1},1),1);   
+%      f01{2} = zeros(size(f1{1},1),1);
+%      f11{1} = ones(size(f1{1},1),1);
+%      f11{2} = ones(size(f1{1},1),1);
+%    end
+%    
+%    if(sz_dim == 3)
+%      %3D  
+%      %Poisson eqn:
+%      g=cos(z)+sin(y)+exp(y).*tanh(x)-exp(y).*tanh(x).^3.*2.0;
+%      
+%      f0{1} =  0*ue -g; 
+%      f1{1} = grad_ue{1};
+%      f1{2} = grad_ue{2};
+%      f1{3} = grad_ue{3};
+% 
+% 
+%      %f_i,0 = partial(f_i)/partial(ue)
+%      f00{1} = zeros(size(f0{1},1),1);
+%      f10{1} = zeros(size(f1{1},1),1); 
+%      f10{2} = zeros(size(f1{2},1),1); 
+%      f10{3} = zeros(size(f1{3},1),1);
+%      
+%      %f_i,1 = partial(f_i)/partial(gradue)
+%      f01{1} = zeros(size(f1{1},1),1);   
+%      f01{2} = zeros(size(f1{2},1),1);
+%      f01{3} = zeros(size(f1{3},1),1);
+%      f11{1} = ones(size(f1{1},1),1);
+%      f11{2} = ones(size(f1{2},1),1);
+%      f11{3} = ones(size(f1{3},1),1);
+%    end
+   
+%==== Poisson ends==================================%   
+   
+   
+%==== Plane Strain Starts============================%
+    
+   %Young's modulus 
+   E = 2e11;
+   % Poisson ratio 
+   nu = 0.3;
+   %strain/stress matrix
+   C =(E/((1+nu)*(1-2*nu)))*[1-nu,nu,0; nu,1-nu,0; 0,0,(1-2*nu)/2];
+    
+   %grad_ue{1} = [partial_u1/partial_x , partial_u2/partial_x ]
+   %grad_ue{2} = [partial_u1/partial_y , partial_u2/partial_y ]
+   
+   %strain = [partial_u1/partial_x, partial_u2/partial_y, 2*(partial_u2/partial_x + partial_u1/partial_y)]   
+   strain = [grad_ue{1}(:,1), grad_ue{2}(:,2), 2*(grad_ue{1}(:,2)+grad_ue{2}(:,1))];     
+    
+   %stress (sigma) = (strain/stress matrix)*strain 
+   sigma = strain*C';
+   
+   %use this
+   g1= -(E.*(nu-1.0./2.0).*(sin(y).*-2.0+exp(y).*tanh(x).*2.0+sin(y).*(tanh(x).^2-1.0).*2.0))./((nu.*2.0-1.0).*(nu+1.0))+(E.*nu.*sin(y).*(tanh(x).^2-1.0))./((nu.*2.0-1.0).*(nu+1.0))-(E.*exp(y).*tanh(x).*(tanh(x).^2-1.0).*(nu-1.0).*2.0)./((nu.*2.0-1.0).*(nu+1.0));
+   g2= (E.*(exp(y).*(tanh(x).^2-1.0).*2.0-cos(y).*tanh(x).*(tanh(x).^2-1.0).*4.0).*(nu-1.0./2.0))./((nu.*2.0-1.0).*(nu+1.0))-(E.*nu.*exp(y).*(tanh(x).^2-1.0))./((nu.*2.0-1.0).*(nu+1.0))+(E.*cos(y).*tanh(x).*(nu-1.0))./((nu.*2.0-1.0).*(nu+1.0));
+   
+   %g1 = ones(4,1)*((E.*(nu-1.0).*-2.0)./((nu.*2.0-1.0).*(nu+1.0))-(E.*(nu-1.0./2.0).*4.0)./((nu.*2.0-1.0).*(nu+1.0)));
+   %g2 = ones(4,1)*((E.*(nu-1.0).*-2.0)./((nu.*2.0-1.0).*(nu+1.0)));
+
+   f0{1} = -g1;
+   f0{2} = -g2;     
+   
+   %use this
+   f1{1} = [sigma(:,1),sigma(:,3)];
+   f1{2} = [sigma(:,3),sigma(:,2)];
+   
+   %f00{1} = [partial_u1/partial_x, partial_u1/partial_y]
+   %f00{2} = [partial_u2/partial_x, partial_u2/partial_y]
+   f00{1} = zeros(size(f0{1},1),2);
+   f00{2} = zeros(size(f0{2},1),2);   
+   
+   f01{1} = zeros(size(f0{1},1),2);
+   f01{2} = zeros(size(f0{2},1),2);
+   
+   f10{1} = zeros(size(f1{2},1),2);
+   f10{2} = zeros(size(f1{2},1),2);
+   
+   one = ones(size(f1{1},1),3);
+   f1_gu = one*C';
+   f11{1}=[f1_gu(:,1),f1_gu(:,3)];
+   f11{2}=[f1_gu(:,3),f1_gu(:,2)]; 
+
+%==== Plane Strain ends============================%
+
+
+% % % 
+% % % 
+% % % %==========test two Poisson starts=================================
+% % % if(sz_dim == 2)
 % % %      %2D  
 % % %      %Poisson eqn:
-% % %      g=sin(y)+exp(y).*tanh(x)-exp(y).*tanh(x).^3.*2.0;
+% % %      g1= sin(y)+exp(y).*tanh(x)-exp(y).*tanh(x).^3.*2.0 ;
+% % %      g2= -cos(y).*tanh(x).*(tanh(x).^2.*2.0-3.0);
 % % %      %==== -\nabla^2 (u) = g  with 0 Dirichlet B.C. ====%
-% % %      f0{1} =  0*ue -g; 
-% % %      f1{1} = grad_ue{1};
-% % %      f1{2} = grad_ue{2};
+% % %      f0{1} =  -g1;
+% % %      f0{2} =  -g2;
+% % %      f1{1} = [grad_ue{1}(:,1),grad_ue{2}(:,1)];
+% % %      f1{2} = [grad_ue{1}(:,2),grad_ue{2}(:,2)];
 % % %      %=====================================================%
 % % %        
 % % %      %f0 = @(ue, grad_ue) ue-g;
@@ -110,82 +220,11 @@ function [f0, f1,f00, f01, f10, f11] = get_userf(ue, grad_ue, xe)
 % % %      f01{2} = zeros(size(f1{1},1),1);
 % % %      f11{1} = ones(size(f1{1},1),1);
 % % %      f11{2} = ones(size(f1{1},1),1);
-% % %    end
-% % %    
-% % %    if(sz_dim == 3)
-% % %      %3D  
-% % %      %Poisson eqn:
-% % %      g=cos(z)+sin(y)+exp(y).*tanh(x)-exp(y).*tanh(x).^3.*2.0;
-% % %      
-% % %      f0{1} =  0*ue -g; 
-% % %      f1{1} = grad_ue{1};
-% % %      f1{2} = grad_ue{2};
-% % %      f1{3} = grad_ue{3};
-% % % 
-% % % 
-% % %      %f_i,0 = partial(f_i)/partial(ue)
-% % %      f00{1} = zeros(size(f0{1},1),1);
-% % %      f10{1} = zeros(size(f1{1},1),1); 
-% % %      f10{2} = zeros(size(f1{2},1),1); 
-% % %      f10{3} = zeros(size(f1{3},1),1);
-% % %      
-% % %      %f_i,1 = partial(f_i)/partial(gradue)
-% % %      f01{1} = zeros(size(f1{1},1),1);   
-% % %      f01{2} = zeros(size(f1{2},1),1);
-% % %      f01{3} = zeros(size(f1{3},1),1);
-% % %      f11{1} = ones(size(f1{1},1),1);
-% % %      f11{2} = ones(size(f1{2},1),1);
-% % %      f11{3} = ones(size(f1{3},1),1);
-% % %    end
-   
-%==== Poisson ends==================================%   
-   
-   
-%==== Plane Strain Starts============================%
-    
-% % %    %Young's modulus 
-% % %    E = 2;
-% % %    % Poisson ratio 
-% % %    nu = 0.3;
-% % %    %strain/stress matrix
-% % %    C =(E/((1+nu)*(1-2*nu)))*[1-nu,nu,0; nu,1-nu,0; 0,0,(1-2*nu)/2];
-% % %     
-% % %    %grad_ue{1} = [partial_u1/partial_x , partial_u2/partial_x ]
-% % %    %grad_ue{2} = [partial_u1/partial_y , partial_u2/partial_y ]
-% % %    
-% % %    %strain = [partial_u1/partial_x, partial_u2/partial_y, 1/2*(partial_u2/partial_x + partial_u1/partial_y)]   
-% % %    strain = [grad_ue{1}(:,1), grad_ue{2}(:,2), 0.5*(grad_ue{1}(:,2)+grad_ue{2}(:,1))];     
-% % %     
-% % %    %stress (sigma) = (strain/stress matrix)*strain 
-% % %    sigma = strain*C';
-% % %    
-% % %    g1= -(E.*(nu-1.0./2.0).*(sin(y).*(-1.0./2.0)+exp(y).*tanh(x).*(1.0./2.0)+sin(y).*(tanh(x).^2-1.0).*(1.0./2.0)))./((nu.*2.0-1.0).*(nu+1.0))+(E.*nu.*sin(y).*(tanh(x).^2-1.0))./((nu.*2.0-1.0).*(nu+1.0))-(E.*exp(y).*tanh(x).*(tanh(x).^2-1.0).*(nu-1.0).*2.0)./((nu.*2.0-1.0).*(nu+1.0));
-% % %    g2= (E.*(exp(y).*(tanh(x).^2-1.0).*(1.0./2.0)-cos(y).*tanh(x).*(tanh(x).^2-1.0)).*(nu-1.0./2.0))./((nu.*2.0-1.0).*(nu+1.0))-(E.*nu.*exp(y).*(tanh(x).^2-1.0))./((nu.*2.0-1.0).*(nu+1.0))+(E.*cos(y).*tanh(x).*(nu-1.0))./((nu.*2.0-1.0).*(nu+1.0));
-% % %    
-% % % 
-% % %    f0{1} = g1;
-% % %    f0{2} = g2;
-% % %     
-% % %    f1{1} = [sigma(:,1),sigma(:,3)] ;
-% % %    f1{2} = [sigma(:,3),sigma(:,2)];
-% % %    
-% % %    %f00{1} = [partial_u1/partial_x, partial_u1/partial_y]
-% % %    %f00{2} = [partial_u2/partial_x, partial_u2/partial_y]
-% % %    f00{1} = zeros(size(f0{1},1),2);
-% % %    f00{2} = zeros(size(f0{2},1),2);   
-% % %    
-% % %    f01{1} = zeros(size(f0{1},1),2);
-% % %    f01{2} = zeros(size(f0{2},1),2);
-% % %    
-% % %    f10{1} = zeros(size(f1{2},1),2);
-% % %    f10{2} = zeros(size(f1{2},1),2);
-% % %    
-% % %    one = ones(size(f1{1},1),3);
-% % %    f1_gu = one*C';
-% % %    f11{1}=[f1_gu(:,1),f1_gu(:,3)];
-% % %    f11{2}=[f1_gu(:,3),f1_gu(:,2)]; 
+% % %  end
 
-%==== Plane Strain ends============================%
+%========test two Poisson ends=============================================
+   
+  
              
 end
 
