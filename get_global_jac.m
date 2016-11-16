cb9af06ff50833421ef456608d84449e16fc0bbb
ArashMehraban <ArashM81@gmail.com>
2016-11-15 21:36:27 -0700

    Returns sparse Jacobian. Needs assembly as a whole. Per element works

--- get_global_jac.m
+++ get_global_jac.m
@@ -1,12 +1,12 @@
-function global_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_in_1d, userdf)
-% GET_GLOBAL_RES evaluates the global residual and the consistent tangent
+function globa_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_in_1d,sz_u_field, userdf)
+% GET_GLOBAL_RES evaluates the actual Jacobian (consistent tangent)
 %  input:              dlta_u: vector of variation of unknowns 
 %       :      global_idx_map: global map of local u's
 %       :                 msh: mesh object 
 %       : num_quadr_pts_in_1d: number of quadrature points in 1D
 %       :              userdf: user supplied code for get_userdf function
 %
-% output: global_jac: the action of consistant tangent on dlta_u
+% output: mfj: Matrix Free Jacobian(mfJ) the action of consistant tangent on dlta_u
 %               
 
      %get the number of elements
@@ -18,13 +18,13 @@ function global_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_i
      %get vertex coordinates
      vtx_coords = msh.vtx_coords;
      
-     %Allocate space for global residual for unkowns     
-     global_jac =zeros(size(dlta_u,1),1);
+     %Allocate space for global Matric Free Jacobian     
+     globa_jac =zeros(size(dlta_u,1),1);
      
-     %get all dirichlet boundary node_sets
-     dir_bndry_nodes = get_all_dir_ns(msh);
-      
-     delta_u_closure = get_closure_dlta_u(dlta_u,dir_bndry_nodes,global_idx_map); 
+%      %get all dirichlet boundary node_sets
+      dir_bndry_nodes = get_all_dir_ns(msh);
+%       
+%      delta_u_closure = get_closure_dlta_u(dlta_u,dir_bndry_nodes,global_idx_map); 
      
      dim = msh.num_dims;                                            
      %get Weights, Basis (B) functions and   [D0]
@@ -41,12 +41,12 @@ function global_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_i
          %get corresponding vertex coordinates for each element 
          element_vtx_coords = vtx_coords(conn(i,:),:);
          
-         %get corresponding unknown/solution u and du for each element
-         elem_dlta_u = delta_u_closure(conn(i,:),:); 
+%          %get corresponding unknown/solution u and du for each element
+%          elem_dlta_u = delta_u_closure(conn(i,:),:); 
          
          %get mapping constituents from element derivative 
-         % 2D: [D1] du/dx
-         %   D=[D2] du/dy
+         % 2D: [D0] du/dx
+         %   D=[D1] du/dy
          %
          % 3D: [D1] du/dx
          %   D=[D2] du/dy
@@ -55,37 +55,50 @@ function global_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_i
          
          %get Gauss Weights for the current element
          W = W_hat.*dets;
-         
-         % grad_dlta_ue structure:
-         % 2D:             [D1*dlta_u1 | D1*dlta_u2] 
-         %    grad_dlta_ue=[D2*dlta_u1 | D2*dlta_u2]
-         %
-         % 3D:             [D1*dlta_u1 | D1*dlta_u2 | D1*dlta_u3]
-         %    grad_dlta_ue=[D2*dlta_u1 | D2*dlta_u2 | D2*dlta_u3]
-         %                 [D3*dlta_u1 | D3*dlta_u2 | D3*dlta_u3]
-         grad_dlta_ue = D*elem_dlta_u;
-         
-         % dlta_ue structure: 
-         %   2D: dlta_ue=[B*u1 | B*u2]   
-         %   3D: dlta_ue=[B*u1 | B*u2 | B*u3]
-         dlta_ue = B*elem_dlta_u;
+%          
+%          % grad_dlta_ue structure:
+%          % 2D:             [D0*dlta_u1 | D0*dlta_u2] 
+%          %    grad_dlta_ue=[D1*dlta_u1 | D1*dlta_u2]
+%          %
+%          % 3D:             [D0*dlta_u1 | D0*dlta_u2 | D0*dlta_u3]
+%          %    grad_dlta_ue=[D1*dlta_u1 | D1*dlta_u2 | D1*dlta_u3]
+%          %                 [D2*dlta_u1 | D2*dlta_u2 | D2*dlta_u3]
+%          grad_dlta_ue = D*elem_dlta_u;
+%          
+%          % dlta_ue structure: 
+%          %   2D: dlta_ue=[B*u1 | B*u2]   
+%          %   3D: dlta_ue=[B*u1 | B*u2 | B*u3]
+%          dlta_ue = B*elem_dlta_u;
                  
          % mapped quadrature points to reference coordinate system
          xe= B*element_vtx_coords;
+        
+         jac_e = zeros(size(B,2)*sz_u_field);
          
-         f = userdf(dlta_ue, grad_dlta_ue,xe);
-         [r,c]=size(f);
-         
-         tmpf = reshape(f, size(W,1), []);
-  
-         for j=1:size(tmpf,2)
-             tmpf(:,j) = W.*tmpf(:,j);
+         for k=1:size(B,2)
+             f = userdf(kron(B(:,k),eye(sz_u_field)), kron(D(:,k),eye(sz_u_field)),xe);
+
+             tmpf = zeros(size(f,1)/sz_u_field, sz_u_field^2);
+             [r,c]=size(tmpf);
+             
+             s=1;
+             for m=1:sz_u_field
+                tmpf(:,1+(m-1)*sz_u_field:m*sz_u_field) = f(s:sz_u_field:end,:);                
+                s=s+1;
+             end
+             
+             tmpf = reshape(tmpf, size(W,1), []);
+
+             for j=1:size(tmpf,2)
+                 tmpf(:,j) = W.*tmpf(:,j);
+             end
+
+             tmpf = reshape(tmpf,r,c);
+             
+             %Jacobian Matrix per element 
+             jac_e(:,1+(k-1)*sz_u_field:k*sz_u_field) = reshape(([B' D']*tmpf)',sz_u_field,[])';         
          end
 
-         tmpf = reshape(tmpf,r,c);
- 
-         jac_e = [B' D']*tmpf;
-                 
          
          % global (the action of) jacobian assembly 
          temp=conn(i,:)';
@@ -93,6 +106,6 @@ function global_jac = get_global_jac(dlta_u, global_idx_map, msh,num_quadr_pts_i
          kk=temp(k);
          in_glb = global_idx_map(kk,:);
          kk =in_glb(in_glb>=0);
-         global_jac(kk) = global_jac(kk)+ jac_e(in_glb>=0);    
+         globa_jac(kk) = globa_jac(kk)+ jac_e(in_glb>=0);    
      end
 end
